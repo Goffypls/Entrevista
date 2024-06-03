ef2e70204ca0ccbb06e9e3e184c7120bf0de547e
Mariano A <antonsichmarian@gmail.com>
2024-06-03 12:53:23 -0300

    stringrevert

---
+++ ReverseString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa una palabra: ");
+            string palabra = Console.ReadLine();
+
+            string palabraInvertida = InvertirPalabra(palabra);
+            Console.WriteLine("Palabra invertida: " + palabraInvertida);
+
+        }
+        static string InvertirPalabra(string palabra)
+        {
+            char[] caracteres = palabra.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
